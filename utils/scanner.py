94aaacb995a15e789de4868463b1436f0a4bcf1a
Andrey Morozov <and.morozov.v@gmail.com>
2025-07-22 14:55:47 +0300

    chore(device): add function for read service and characteristics

--- utils/scanner.py
+++ utils/scanner.py
@@ -6,6 +6,9 @@ from bleak import AdvertisementData, BLEDevice, BleakScanner
 
 DEFAULT_TIMEOUT = 30
 
+# NAME_TEMPLATE = "EMG-SENS"
+NAME_TEMPLATE = "inRat"
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(
         level=logging.DEBUG,
@@ -14,18 +17,39 @@ logging.basicConfig(
 
 async def find_device(
         timeout: int | None = None,
-        event_stop: asyncio.Event | None = None,
+        template: str = NAME_TEMPLATE
 ) -> tuple[BLEDevice, AdvertisementData] | tuple[None, None]:
+    """
+    Find ble device on template.
+    :param timeout: int
+    :param template: str
+    :return: BLEDevice, AdvertisementData
+    """
     async with BleakScanner() as scanner:
+
         with contextlib.suppress(asyncio.TimeoutError):
+
             async with asyncio.timeout(timeout):
+
                 async for device, advertisement in scanner.advertisement_data():
 
-                    # if event_stop.is_set():
-                    #     break
-
-                    if device is not None and device.name is not None and device.name.startswith("EMG-SENS"):
+                    if device is not None and device.name is not None and device.name.startswith(template):
                         return device, advertisement
     return None, None
 
-asyncio.run(find_device())
+
+async def discover(
+        wait: int = 3,
+) -> list[tuple[BLEDevice, AdvertisementData]]:
+    """
+    Discover ble devices in network.
+    :param wait: int
+    :return: (BLEDevice, AdvertisementData)
+    """
+    async with BleakScanner() as scanner:
+        await asyncio.sleep(wait)
+        return [(d,a) for (d,a) in scanner.discovered_devices_and_advertisement_data.values()]
+
+if __name__ == "__main__":
+    # asyncio.run(find_device())
+    asyncio.run(discover())
