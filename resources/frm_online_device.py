885bbb435fbb8a2d3f6137b2f8305c06c2f14f72
Andrey Morozov <and.morozov.v@gmail.com>
2026-01-31 16:47:30 +0300

    update ui for control panel device

--- resources/frm_online_device.py
+++ resources/frm_online_device.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'frm_online_devicekhDXmf.ui'
+## Form generated from reading UI file 'frm_online_deviceXQojNI.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.0
 ##
@@ -16,56 +16,75 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QGroupBox,
-    QPushButton, QSizePolicy, QVBoxLayout, QWidget)
+    QPushButton, QSizePolicy, QWidget)
 
 class Ui_FrmDevice(object):
     def setupUi(self, FrmDevice):
         if not FrmDevice.objectName():
             FrmDevice.setObjectName(u"FrmDevice")
-        FrmDevice.resize(337, 177)
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(FrmDevice.sizePolicy().hasHeightForWidth())
-        FrmDevice.setSizePolicy(sizePolicy)
-        self.gridLayout = QGridLayout(FrmDevice)
+        FrmDevice.resize(380, 243)
+        self.gridLayout_3 = QGridLayout(FrmDevice)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.gridLayout = QGridLayout()
         self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout.setContentsMargins(5, 5, 5, 5)
         self.groupBox = QGroupBox(FrmDevice)
         self.groupBox.setObjectName(u"groupBox")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Expanding)
-        sizePolicy1.setHorizontalStretch(0)
-        sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.groupBox.sizePolicy().hasHeightForWidth())
-        self.groupBox.setSizePolicy(sizePolicy1)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.groupBox.sizePolicy().hasHeightForWidth())
+        self.groupBox.setSizePolicy(sizePolicy)
+        self.groupBox.setMinimumSize(QSize(350, 180))
         font = QFont()
         font.setPointSize(12)
         self.groupBox.setFont(font)
-        self.verticalLayoutWidget = QWidget(self.groupBox)
-        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
-        self.verticalLayoutWidget.setGeometry(QRect(10, 20, 301, 131))
-        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.pushButtonStart = QPushButton(self.verticalLayoutWidget)
+        self.gridLayout_2 = QGridLayout(self.groupBox)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.gridLayout_4 = QGridLayout()
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
+        self.gridLayout_4.setContentsMargins(10, 10, 10, 10)
+        self.pushButtonStart = QPushButton(self.groupBox)
         self.pushButtonStart.setObjectName(u"pushButtonStart")
         self.pushButtonStart.setEnabled(False)
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.pushButtonStart.sizePolicy().hasHeightForWidth())
+        self.pushButtonStart.setSizePolicy(sizePolicy1)
+        self.pushButtonStart.setMinimumSize(QSize(0, 40))
+        self.pushButtonStart.setFont(font)
 
-        self.verticalLayout.addWidget(self.pushButtonStart)
+        self.gridLayout_4.addWidget(self.pushButtonStart, 0, 0, 1, 1)
 
-        self.pushButtonStop = QPushButton(self.verticalLayoutWidget)
+        self.pushButtonStop = QPushButton(self.groupBox)
         self.pushButtonStop.setObjectName(u"pushButtonStop")
         self.pushButtonStop.setEnabled(False)
+        sizePolicy1.setHeightForWidth(self.pushButtonStop.sizePolicy().hasHeightForWidth())
+        self.pushButtonStop.setSizePolicy(sizePolicy1)
+        self.pushButtonStop.setMinimumSize(QSize(0, 40))
+        self.pushButtonStop.setFont(font)
 
-        self.verticalLayout.addWidget(self.pushButtonStop)
+        self.gridLayout_4.addWidget(self.pushButtonStop, 1, 0, 1, 1)
 
-        self.pushButtonDisconnect = QPushButton(self.verticalLayoutWidget)
+        self.pushButtonDisconnect = QPushButton(self.groupBox)
         self.pushButtonDisconnect.setObjectName(u"pushButtonDisconnect")
         self.pushButtonDisconnect.setEnabled(False)
+        sizePolicy1.setHeightForWidth(self.pushButtonDisconnect.sizePolicy().hasHeightForWidth())
+        self.pushButtonDisconnect.setSizePolicy(sizePolicy1)
+        self.pushButtonDisconnect.setMinimumSize(QSize(0, 40))
+        self.pushButtonDisconnect.setFont(font)
 
-        self.verticalLayout.addWidget(self.pushButtonDisconnect)
+        self.gridLayout_4.addWidget(self.pushButtonDisconnect, 2, 0, 1, 1)
+
+
+        self.gridLayout_2.addLayout(self.gridLayout_4, 0, 0, 1, 1)
 
 
         self.gridLayout.addWidget(self.groupBox, 0, 0, 1, 1)
+
+
+        self.gridLayout_3.addLayout(self.gridLayout, 0, 0, 1, 1)
 
 
         self.retranslateUi(FrmDevice)
@@ -75,7 +94,7 @@ class Ui_FrmDevice(object):
 
     def retranslateUi(self, FrmDevice):
         FrmDevice.setWindowTitle(QCoreApplication.translate("FrmDevice", u"Frame", None))
-        self.groupBox.setTitle(QCoreApplication.translate("FrmDevice", u"InRat", None))
+        self.groupBox.setTitle(QCoreApplication.translate("FrmDevice", u"inRat", None))
         self.pushButtonStart.setText(QCoreApplication.translate("FrmDevice", u"\u0421\u0442\u0430\u0440\u0442", None))
         self.pushButtonStop.setText(QCoreApplication.translate("FrmDevice", u"\u0421\u0442\u043e\u043f", None))
         self.pushButtonDisconnect.setText(QCoreApplication.translate("FrmDevice", u"\u041e\u0442\u043a\u043b\u044e\u0447\u0438\u0442\u044c", None))
