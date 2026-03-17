8592718c0d0af8d65cc2382055eca816466790ee
Andrey Morozov <and.morozov.v@gmail.com>
2026-03-17 14:29:33 +0300

    feat: add script generate exe file with pyinstaller

---
+++ build.py
@@ -0,0 +1,11 @@
+import PyInstaller.__main__
+
+PyInstaller.__main__.run([
+    "main.py",
+    "--onefile",
+    "--windowed",
+    # "--console",
+    "--name=inRat monitor",
+    "--clean",
+    "--icon=ui/iconMCS.ico",
+])
